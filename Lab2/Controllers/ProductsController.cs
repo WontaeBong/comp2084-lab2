39864cb2f6a9bd36801790a5235a7ada0c9e509f
Wontae Bong <200366428@student.georgianc.on.ca>
2018-09-28 20:36:38 -0400

    add controller, view

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello Mike";
+
+            return View();
+        }
+
+
+        // GET: Store/Product
+        public ActionResult Product(string ProductName)
+        {
+            ViewBag.ProductName = ProductName;
+            return View();
+        }
+    }
+}
